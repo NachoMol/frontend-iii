2fd013f666a4c6c8f2fb37a228729b79e4e05814
NachoMol <ignaciotmolina@gmail.com>
2023-08-06 20:31:36 -0300

    eliminando acceso directo al otro repo

---
+++ ctd-fe3-2023-mesa-5-main/src/App.jsx
@@ -0,0 +1,51 @@
+const productos = [
+  {
+      "id": 0,
+      "nombre": "Macbook Air 13 Chip M1 256gb",
+      "imagen": "https://m.media-amazon.com/images/I/41O807iqbCL._AC_SX522_.jpg",
+      "precio": "$ 1499.00"
+  },
+  {
+      "id": 1,
+      "nombre": "Amazon Echo Dot (4a Gen.)", 
+      "imagen": "https://m.media-amazon.com/images/I/714Rq4k05UL._AC_SL1000_.jpg",
+      "precio": "$ 14.99"
+  },
+  {
+      "id": 2,
+      "nombre": "Cámara IP Inalámbrica 360°",
+      "imagen": "https://m.media-amazon.com/images/I/51F70OM213S._AC_SL1000_.jpg",
+      "precio": "$ 39.00"
+  },
+  {
+      "id": 3,
+      "nombre": "CERRADURA DIGITAL INTELIGENTE FORTEZZA ZIGBEE",
+      "imagen": "https://m.media-amazon.com/images/I/51RXeqMLceL._AC_SL1500_.jpg",
+      "precio": "$ 99.00"
+  }
+]
+
+//probando a ver si puedo hacer commits sobre este repo
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1>Productos</h1>
+      {productos.map((e)=> {
+        return(
+          <ul key={e.id}>
+          <li>
+            <h2>{e.nombre}</h2>
+            <img src={e.imagen} alt="imagen" />
+            <h3>{e.precio}</h3>
+          </li>
+          </ul>
+        )
+      })}
+    </div>
+    </>
+  );
+}
+
+export default App;
