4e670f2ca5a8381bbce5536e3a30c96887b3d4f5
NachoMol <ignaciotmolina@gmail.com>
2023-08-09 20:46:41 -0300

    subiendo clase 6s - ejercicio pokemons

---
+++ vite-clase6s/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import Home from './Pages/Home'
+import Navbar from './Components/Navbar/Navbar'
+import { useState } from 'react'
+import Cart from './Components/Cart/Cart'
+import Mesa5 from './Mesa5'
+import Form from './Components/Form/Form'
+import Pokemons from './Components/Pokemons/Pokemons'
+
+
+function App() {
+  const [cart, setCart] = useState([])
+
+  const add = (item) => {
+    setCart([...cart, item])
+  }
+  console.log(cart)
+  return (
+    <div className='App'>
+      {/* <Mesa5/> */}
+      <Navbar/>
+      <Cart cart={cart}/>
+      {/* <Form/> */}
+      <Pokemons />
+      <Home add={add}/>
+    </div>
+  )
+}
+
+export default App
