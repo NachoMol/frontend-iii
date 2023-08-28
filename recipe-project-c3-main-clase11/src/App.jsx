e6ad88b3b0d17ed199233cf61ca196f3ed189997
NachoMol <ignaciotmolina@gmail.com>
2023-08-28 20:21:05 -0300

    subiendo clase 11 profe

---
+++ recipe-project-c3-main-clase11/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Navbar from './Components/Navbar'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Detalle from './Pages/Detalle'
+import Home from './Pages/Home'
+
+
+
+
+function App() {
+ 
+  
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/element/:elemento' element={<Detalle/>}/>
+      </Routes>
+   </>
+  )
+}
+
+export default App
