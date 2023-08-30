b3b8d636b60dfccc5b924c609719b118026fca8a
NachoMol <ignaciotmolina@gmail.com>
2023-08-30 20:45:43 -0300

    implementando Form

--- ejercicio-clase12-mesaTrabajo/src/Home.jsx
+++ ejercicio-clase12-mesaTrabajo/src/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Form from './Components/Form'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Formulario</h1>
+        <Form/>
+    </div>
   )
 }
 
